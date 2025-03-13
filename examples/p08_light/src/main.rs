e1bbc6dfa5a555517a25d485f771d9c739830a15
GlummixX <mateshrac@gmail.com>
2025-03-13 11:17:20 +0100

    wip phong + doc

--- examples/p08_light/src/main.rs
+++ examples/p08_light/src/main.rs
@@ -4,11 +4,35 @@ use bevy::{
     render::{
         settings::{Backends, RenderCreation, WgpuSettings},
         RenderPlugin,
+        mesh::{MeshVertexAttribute, PrimitiveTopology},
+        render_resource::{AsBindGroup, ShaderRef, VertexFormat},
     }, window::PrimaryWindow,
 };
 use bevy_obj::ObjPlugin;
 
 //https://sketchfab.com/3d-models/the-utah-teapot-1092c2832df14099807f66c8b792374d
+
+const SHADER_FILE: &str = "../src/shader.wgsl";
+
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct PhongLight {
+    #[uniform(0)]
+    light_pos: Vec3,
+    #[uniform(1)]
+    camera_pos: Vec3,
+    #[uniform(2)]
+    ambient_color: Vec3,
+    #[uniform(3)]
+    light_color: Vec3,
+    #[uniform(4)]
+    specular_value: f32,
+}
+
+impl Material for PhongLight {
+    fn fragment_shader() -> ShaderRef {
+        SHADER_FILE.into()
+    }
+}
 
 fn main() {
     let render_plugin = RenderPlugin {
@@ -21,6 +45,7 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(render_plugin));
     app.add_plugins(ObjPlugin);
+    app.add_plugins(MaterialPlugin::<PhongLight>::default());
     app.add_systems(Startup, setup);
     app.add_systems(Update, camera_controller_system);
     app.add_systems(Update, esc_exit_system);
@@ -82,25 +107,25 @@ fn camera_controller_system(
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut materials: ResMut<Assets<PhongLight>>,
     asset_server: Res<AssetServer>,
 ) {
     // Create camera
     commands.spawn((Camera3d::default(), Transform::from_xyz(0., 0., 20.)));
-    // Create light
-    commands.spawn((
-        DirectionalLight{
-            shadows_enabled:true,
-            illuminance: 1500.0,
-            ..Default::default()
-        },
-        Transform::from_xyz(25.0, 30.0, 35.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
+
+    // Create Phong 
+    let phong = PhongLight {
+        light_pos: Vec3::new(5.0, 5.0, -5.0),
+        camera_pos: Vec3::new(0.0, 0.0, 0.0),
+        ambient_color: Vec3::splat(0.2),
+        light_color: Vec3::new(1.0, 1.0, 1.0),
+        specular_value: 32.0,
+    };
 
     let mesh = Mesh3d(asset_server.load("teapot.obj"));
     commands.spawn((
         mesh,
-        MeshMaterial3d(materials.add(Color::srgb(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(phong)),
         Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(0.05)),
     ));
 }
