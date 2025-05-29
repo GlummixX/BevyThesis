0dec058e0959d86566fc35609bad94e967a32f1a
GlummixX <mateshrac@gmail.com>
2025-05-29 20:19:01 +0200

    fmt + cam plugin

--- examples/p13_post_processing/src/main.rs
+++ examples/p13_post_processing/src/main.rs
@@ -6,17 +6,22 @@ use bevy::{
     ecs::query::QueryItem,
     prelude::*,
     render::{
-        extract_component::{
-            ComponentUniforms, DynamicUniformIndex, ExtractComponent, ExtractComponentPlugin,
-            UniformComponentPlugin,
-        }, render_graph::{
+        render_graph::{
             NodeRunError, RenderGraphApp, RenderGraphContext, RenderLabel, ViewNode, ViewNodeRunner,
-        }, render_resource::{
-            binding_types::{sampler, texture_2d, uniform_buffer},
+        },
+        render_resource::{
+            binding_types::{sampler, texture_2d},
             *,
-        }, renderer::{RenderContext, RenderDevice}, settings::{Backends, RenderCreation, WgpuSettings}, view::ViewTarget, MainWorld, RenderApp, RenderPlugin
-    }, window::{PresentMode, WindowMode, WindowResolution},
+        },
+        renderer::{RenderContext, RenderDevice},
+        settings::{Backends, RenderCreation, WgpuSettings},
+        view::ViewTarget,
+        MainWorld, RenderApp, RenderPlugin,
+    },
+    window::{PresentMode, WindowMode, WindowResolution},
 };
+
+use camera_plugin;
 
 const SCANLINE_SHADER: &str = "scanline.wgsl";
 const SEPIA_SHADER: &str = "sepia.wgsl";
@@ -45,19 +50,71 @@ fn main() {
     };
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(window_plugin).set(render_plugin));
-    app.add_plugins(PostProcessPlugin);
+    app.add_plugins((PostProcessPlugin, camera_plugin::CameraControlPlugin));
     app.add_systems(Startup, setup);
     app.add_systems(Update, rotate);
     app.run();
 }
 
-/// It is generally encouraged to set up post processing effects as a plugin
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 5.0)).looking_at(Vec3::default(), Vec3::Y),
+        camera_plugin::ControlledCamera,
+    ));
+
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::default())),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.1, 0.6))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+        Rotates,
+    ));
+
+    // light
+    commands.spawn(DirectionalLight {
+        illuminance: 1000.,
+        ..default()
+    });
+}
+
+#[derive(Component)]
+struct Rotates;
+
+fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in &mut query {
+        transform.rotate_x(0.55 * time.delta_secs());
+        transform.rotate_z(0.15 * time.delta_secs());
+    }
+}
+
+#[derive(Event)]
+struct SwitchShaderEvent;
+
+fn shader_switcher(
+    mut events: EventWriter<SwitchShaderEvent>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        events.send(SwitchShaderEvent);
+    }
+}
+
+/// ==== Post process plugin ==== ///
+
 struct PostProcessPlugin;
 
 fn check_event(mut world: ResMut<MainWorld>, mut ppp: ResMut<PostProcessPipeline>) {
-    let mut events = world.get_resource_mut::<Events<SwitchShaderEvent>>().unwrap();
+    let mut events = world
+        .get_resource_mut::<Events<SwitchShaderEvent>>()
+        .unwrap();
     let event = !events.is_empty();
-    if event{
+    if event {
         events.clear();
         ppp.cycle_active();
     };
@@ -67,39 +124,21 @@ impl Plugin for PostProcessPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<SwitchShaderEvent>();
         app.add_systems(Update, shader_switcher);
-        
+
         // We need to get the render app from the main app
         let Some(render_app) = app.get_sub_app_mut(RenderApp) else {
             return;
         };
 
-
         render_app.add_systems(ExtractSchedule, check_event);
 
         render_app
-            // Bevy's renderer uses a render graph which is a collection of nodes in a directed acyclic graph.
-            // It currently runs on each view/camera and executes each node in the specified order.
-            // It will make sure that any node that needs a dependency from another node
-            // only runs when that dependency is done.
-            //
-            // Each node can execute arbitrary work, but it generally runs at least one render pass.
-            // A node only has access to the render world, so if you need data from the main world
-            // you need to extract it manually or with the plugin like above.
-            // Add a [`Node`] to the [`RenderGraph`]
-            // The Node needs to impl FromWorld
-            //
-            // The [`ViewNodeRunner`] is a special [`Node`] that will automatically run the node for each view
-            // matching the [`ViewQuery`]
             .add_render_graph_node::<ViewNodeRunner<PostProcessNode>>(
-                // Specify the label of the graph, in this case we want the graph for 3d
                 Core3d,
-                // It also needs the label of the node
                 PostProcessLabel,
             )
             .add_render_graph_edges(
                 Core3d,
-                // Specify the node ordering.
-                // This will automatically create all required node edges to enforce the given ordering.
                 (
                     Node3d::Tonemapping,
                     PostProcessLabel,
@@ -109,39 +148,24 @@ impl Plugin for PostProcessPlugin {
     }
 
     fn finish(&self, app: &mut App) {
-        // We need to get the render app from the main app
         let Some(render_app) = app.get_sub_app_mut(RenderApp) else {
             return;
         };
 
-        render_app
-            // Initialize the pipeline
-            .init_resource::<PostProcessPipeline>();
+        render_app.init_resource::<PostProcessPipeline>();
     }
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
 struct PostProcessLabel;
 
-// The post process node used for the render graph
+// Post process node for the render graph
 #[derive(Default)]
 struct PostProcessNode;
 
-// The ViewNode trait is required by the ViewNodeRunner
 impl ViewNode for PostProcessNode {
-    // The node needs a query to gather data from the ECS in order to do its rendering,
-    // but it's not a normal system so we need to define it manually.
-    //
-    // This query will only run on the view entity
     type ViewQuery = &'static ViewTarget;
 
-    // Runs the node logic
-    // This is where you encode draw commands.
-    //
-    // This will run on every view on which the graph is running.
-    // If you don't want your effect to run on every camera,
-    // you'll need to make sure you have a marker component as part of [`ViewQuery`]
-    // to identify which camera(s) should run the effect.
     fn run(
         &self,
         _graph: &mut RenderGraphContext,
@@ -149,60 +173,32 @@ impl ViewNode for PostProcessNode {
         view_target: QueryItem<Self::ViewQuery>,
         world: &World,
     ) -> Result<(), NodeRunError> {
-        // Get the pipeline resource that contains the global data we need
-        // to create the render pipeline
         let post_process_pipeline = world.resource::<PostProcessPipeline>();
-
-        // The pipeline cache is a cache of all previously created pipelines.
-        // It is required to avoid creating a new pipeline each frame,
-        // which is expensive due to shader compilation.
         let pipeline_cache = world.resource::<PipelineCache>();
 
-        let Some(pipeline_def) = post_process_pipeline.get_active()
-        else{
+        let Some(pipeline_def) = post_process_pipeline.get_active() else {
             return Ok(());
         };
 
-        // Get the pipeline from the cache
-        let Some(pipeline) = pipeline_cache.get_render_pipeline(pipeline_def.pipeline_id)
-        else {
+        let Some(pipeline) = pipeline_cache.get_render_pipeline(pipeline_def.pipeline_id) else {
             return Ok(());
         };
 
-        // This will start a new "post process write", obtaining two texture
-        // views from the view target - a `source` and a `destination`.
-        // `source` is the "current" main texture and you _must_ write into
-        // `destination` because calling `post_process_write()` on the
-        // [`ViewTarget`] will internally flip the [`ViewTarget`]'s main
-        // texture to the `destination` texture. Failing to do so will cause
-        // the current main texture information to be lost.
         let post_process = view_target.post_process_write();
 
-        // The bind_group gets created each frame.
-        //
-        // Normally, you would create a bind_group in the Queue set,
-        // but this doesn't work with the post_process_write().
-        // The reason it doesn't work is because each post_process_write will alternate the source/destination.
-        // The only way to have the correct source/destination for the bind_group
-        // is to make sure you get it during the node execution.
         let bind_group = render_context.render_device().create_bind_group(
             "post_process_bind_group",
             &pipeline_def.layout,
-            // It's important for this to match the BindGroupLayout defined in the PostProcessPipeline
             &BindGroupEntries::sequential((
-                // Make sure to use the source view
                 post_process.source,
-                // Use the sampler created for the pipeline
                 &pipeline_def.sampler,
             )),
         );
 
-        // Begin the render pass
+        // Rrender pass
         let mut render_pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
             label: Some("post_process_pass"),
             color_attachments: &[Some(RenderPassColorAttachment {
-                // We need to specify the post process destination view here
-                // to make sure we write to the appropriate texture.
                 view: post_process.destination,
                 resolve_target: None,
                 ops: Operations::default(),
@@ -212,8 +208,7 @@ impl ViewNode for PostProcessNode {
             occlusion_query_set: None,
         });
 
-        // This is mostly just wgpu boilerplate for drawing a fullscreen triangle,
-        // using the pipeline/bind_group created above
+        // drawing a fullscreen triangle,
         render_pass.set_render_pipeline(pipeline);
         render_pass.set_bind_group(0, &bind_group, &[]);
         render_pass.draw(0..3, 0..1);
@@ -222,8 +217,7 @@ impl ViewNode for PostProcessNode {
     }
 }
 
-// This contains global data used by the render pipeline. This will be created once on startup.
-struct PipelineDef{
+struct PipelineDef {
     layout: BindGroupLayout,
     sampler: Sampler,
     pipeline_id: CachedRenderPipelineId,
@@ -247,26 +241,23 @@ impl FromWorld for PostProcessPipeline {
     }
 }
 
-impl PostProcessPipeline{
-    fn get_active(&self)->Option<&PipelineDef>{
+impl PostProcessPipeline {
+    fn get_active(&self) -> Option<&PipelineDef> {
         self.defs.get(self.active)
     }
-    fn cycle_active(&mut self){
-        self.active+=1;
-        if self.active >= self.defs.len(){
+
+    fn cycle_active(&mut self) {
+        self.active += 1;
+        if self.active >= self.defs.len() {
             self.active = 0;
         }
-        println!("{}", self.active);
     }
 
     fn add_pipeline(world: &mut World, shader: String) -> PipelineDef {
         let render_device = world.resource::<RenderDevice>();
-
-        // We need to define the bind group layout used for our pipeline
         let layout = render_device.create_bind_group_layout(
             "post_process_bind_group_layout",
             &BindGroupLayoutEntries::sequential(
-                // The layout entries will only be visible in the fragment stage
                 ShaderStages::FRAGMENT,
                 (
                     // The screen texture
@@ -277,25 +268,18 @@ impl PostProcessPipeline{
             ),
         );
 
-        // We can create the sampler here since it won't change at runtime and doesn't depend on the view
         let sampler = render_device.create_sampler(&SamplerDescriptor::default());
-
-        // Get the shader handle
         let shader = world.load_asset(shader);
 
         let pipeline_id = world
             .resource_mut::<PipelineCache>()
-            // This will add the pipeline to the cache and queue its creation
             .queue_render_pipeline(RenderPipelineDescriptor {
                 label: Some("post_process_pipeline".into()),
                 layout: vec![layout.clone()],
-                // This will setup a fullscreen triangle for the vertex state
                 vertex: fullscreen_shader_vertex_state(),
                 fragment: Some(FragmentState {
                     shader,
                     shader_defs: vec![],
-                    // Make sure this matches the entry point of your shader.
-                    // It can be anything as long as it matches here and in the shader.
                     entry_point: "fragment".into(),
                     targets: vec![Some(ColorTargetState {
                         format: TextureFormat::bevy_default(),
@@ -303,8 +287,6 @@ impl PostProcessPipeline{
                         write_mask: ColorWrites::ALL,
                     })],
                 }),
-                // All of the following properties are not important for this effect so just use the default values.
-                // This struct doesn't have the Default trait implemented because not all fields can have a default value.
                 primitive: PrimitiveState::default(),
                 depth_stencil: None,
                 multisample: MultisampleState::default(),
@@ -312,59 +294,10 @@ impl PostProcessPipeline{
                 zero_initialize_workgroup_memory: false,
             });
 
-        PipelineDef{
+        PipelineDef {
             layout,
             sampler,
             pipeline_id,
         }
-    }
-}
-
-/// Set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_translation(Vec3::new(0.0, 0.0, 5.0)).looking_at(Vec3::default(), Vec3::Y),
-    ));
-
-    // cube
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.1, 0.6))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-        Rotates,
-    ));
-    // light
-    commands.spawn(DirectionalLight {
-        illuminance: 1_000.,
-        ..default()
-    });
-}
-
-#[derive(Component)]
-struct Rotates;
-
-/// Rotates any entity around the x and y axis
-fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
-    for mut transform in &mut query {
-        transform.rotate_x(0.55 * time.delta_secs());
-        transform.rotate_z(0.15 * time.delta_secs());
-    }
-}
-
-#[derive(Event)]
-struct SwitchShaderEvent;
-
-fn shader_switcher(
-    mut events: EventWriter<SwitchShaderEvent>,
-    keyboard: Res<ButtonInput<KeyCode>>,
-) {
-    if keyboard.just_pressed(KeyCode::Space) {
-        events.send(SwitchShaderEvent);
     }
 }
